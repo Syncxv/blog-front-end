102440502ca0c83a9b6288d5594736e2b847b845
Syncxv <aruldevarackal1@gmail.com>
2022-05-12 20:19:57 +0530

    blog dumb component gg

--- src/components/BlogFeed/Blog.tsx
+++ src/components/BlogFeed/Blog.tsx
@@ -1,11 +1,17 @@
+import Button from "../atoms/Button";
+
 interface Props {}
 
 const Blog: React.FC<Props> = () => {
     return (
-        <div className="blog p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <a href="#">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-            </a>
+        <div className="max-w-md p-6 rounded-lg text-gray-200 bg-gray-900 dark:bg-gray-100 dark:text-gray-900">
+            <span className="text-sm text-gray-400">1 April 2020</span>
+            <h3 className="text-3xl font-semibold mt-1">Architechture Business</h3>
+            <p className="description my-4 text-base font-medium">
+                a nice long description Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur odit tempora beatae deleniti obcaecati
+                neque laborum ad repellat veritatis nulla?
+            </p>
+            <Button className="mt-4">Read More</Button>
         </div>
     );
 };
